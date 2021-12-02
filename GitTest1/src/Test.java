883d665348e4f09fac582d5287ff6c05bf080139
smhrd <smhrd@DESKTOP-RQ8VIRQ>
2021-12-02 16:16:08 +0900

    commit

--- GitTest1/src/Test.java
+++ GitTest1/src/Test.java
@@ -2,10 +2,7 @@
 public class Test {
 
 	public static void main(String[] args) {
-		// Github 환경설정
-		// 1. JDK 버전
-		// 2. Tomcat 버전
-		// 3. Encoding 방식(UTF-8)
+		
 		System.out.println("first");
 	}
 
